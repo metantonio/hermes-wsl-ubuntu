346568f100674d2cab507ff488f551bdbac8b6d2
metantonio <antonio_martinez88@hotmail.com>
2026-04-07 09:55:16 +0200

    upd

--- setup-wsl.sh
+++ setup-wsl.sh
@@ -109,8 +109,9 @@ echo "2) Qwen3.5-9B-Q5_K_M.gguf (6.5 GB)"
 echo "3) Omnicoder:9B-Q4_K_M.gguf (6.52 GB)"
 echo "4) Gemma4:E4B-Q4_K_M.gguf (4.98 GB)"
 echo "5) Carnice-9b-GGUF-Q6_K.gguf (7.36 GB) (Fine-tuned for Hermes)"
-echo "6) Skip"
-read -p "Choose [1-5]: " choice
+echo "6) Carnice-9b-GGUF-Q4_K_M.gguf (6.50 GB) (Fine-tuned for Hermes)"
+echo "7) Skip"
+read -p "Choose [1-7]: " choice
 
 if [ "$choice" == "1" ]; then
     echo "Downloading model..."
@@ -135,6 +136,11 @@ fi
 if [ "$choice" == "5" ]; then
     echo "Downloading model..."
     wget https://huggingface.co/kai-os/Carnice-9b-GGUF/resolve/main/Carnice-9b-Q6_K.gguf
+fi
+
+if [ "$choice" == "6" ]; then
+    echo "Downloading model..."
+    wget https://huggingface.co/kai-os/Carnice-9b-GGUF/resolve/main/Carnice-9b-Q4_K_M.gguf
 fi
 
 # ----------------------------
